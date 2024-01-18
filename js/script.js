21bdf607d020944880aa3529073ada20579454b1
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-01-18 14:32:21 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,44 +1,36 @@
-let tela = document.getElementById('resultado');
+let resultadoElement = document.getElementById('resultado');
+let expressao = '';
+let ultimoCaracterOperador = false;
 
 function appendToResult(valor) {
-    if (tela.value === '0' && valor !== '.') {
-        tela.value = valor;
+    if (isOperador(valor)) {
+        if (!ultimoCaracterOperador) {
+            expressao += valor;
+            ultimoCaracterOperador = true;
+        }
     } else {
-        // Verifica se o último caractere na tela é um operador
-        const ultimoCaractere = tela.value.charAt(tela.value.length - 1)
-        const operadores = ['+', '-', '*', '/'] // Adicione outros operadores, se necessário
-
-        const operadorExibicao = {
-            '+' : '+',
-            '-' : '-',
-            '*' : 'x',
-            '/' : '÷'
-        }
-
-        if (operadores.includes(ultimoCaractere) && operadores.includes(valor)) {
-            // Substitui o último operador pelo novo operador
-            tela.value = tela.value.slice(0, -1) + valor
-        } else {
-            // Adiciona espaço antes do operador se necessário
-            if (operadores.includes(valor)) {
-                tela.value += ' ' + operadorExibicao[valor] + ' '
-            } else {
-                tela.value += valor
-            }
-        }
+        expressao += valor;
+        ultimoCaracterOperador = false;
     }
+    
+    resultadoElement.value = expressao;
+}
+
+function isOperador(valor) {
+    return ['+', '-', '*', '/'].includes(valor);
 }
 
 function limpar() {
-    tela.value = '0';
+    expressao = '';
+    resultadoElement.value = '0';
+    ultimoCaracterOperador = false;
 }
 
 function calcular() {
     try {
-        // Substitui '÷' por '/' e 'x' por '*' antes de avaliar a expressão
-        const expressao = tela.value.replace(/÷/g, '/').replace(/x/g, '*')
-        tela.value = eval(expressao);
+        expressao = eval(expressao).toString();
+        resultadoElement.value = expressao;
     } catch (error) {
-        tela.value = 'Erro';
+        resultadoElement.value = 'Erro';
     }
 }
